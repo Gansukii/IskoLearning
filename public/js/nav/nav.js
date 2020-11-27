1f269b1fe8424a404b0d50b704af82d2518543a4
Ragan <lamocragan@gmail.com>
2020-11-27 12:15:04 -0800

    profile frontend done2.0

--- public/js/nav/nav.js
+++ public/js/nav/nav.js
@@ -1,56 +1,58 @@
-const search = document.getElementById('search');
-const avatar = document.getElementById('avatar');
-const menuPop = document.getElementById('menuPop');
-const signOut = document.getElementById('sign-out');
+const search = document.getElementById("search");
+const avatar = document.getElementById("avatar");
+const menuPop = document.getElementById("menuPop");
+const signOut = document.getElementById("sign-out");
 let isOpen = false;
 var lg = 992;
 
+// $(document).click(function (event) {
+//   console.log(isOpen);
+//   console.log(menuPop);
+// });
+
 window.onresize = () => {
-    if (document.documentElement.clientWidth < lg) {
-        if (!search.classList.contains('col-8')) {
-            search.classList.add('col-8');
-            avatar.classList.add()
-            menuPop.style.left = "-100px";
-        }
-    } else {
-        search.classList.remove('col-8');
-        menuPop.removeAttribute("style")
-    }
-}
+  if (document.documentElement.clientWidth < lg) {
+    menuPop.style.left = "-100px";
+  } else {
+    menuPop.removeAttribute("style");
+  }
+};
 if (document.documentElement.clientWidth < lg) {
-    if (document.documentElement.clientWidth < lg) {
-        if (!search.classList.contains('col-8')) {
-            search.classList.add('col-8');
-            avatar.classList.add()
-            menuPop.style.left = "-100px";
-        }
-    } else {
-        search.classList.remove('col-8');
-        menuPop.removeAttribute("style")
-    }
+  if (document.documentElement.clientWidth < lg) {
+    menuPop.style.left = "-100px";
+  } else {
+    menuPop.removeAttribute("style");
+  }
 }
 
-avatar ? avatar.onclick = () => {
-
-    if (!isOpen) {
+avatar
+  ? (avatar.onclick = () => {
+      console.log("ngak");
+      if (!isOpen) {
         menuPop.classList.remove("d-none");
         menuPop.classList.remove("menuClose");
         menuPop.classList.add("d-flex");
         menuPop.classList.add("menuOpen");
         isOpen = true;
-    } else {
+      } else {
         menuPop.classList.remove("menuOpen");
         menuPop.classList.add("menuClose");
         menuPop.classList.add("d-none");
         isOpen = false;
-    }
-} : null
+      }
+    })
+  : null;
 
-signOut ? signOut.onclick = () => {
-        firebase.auth().signOut().then(function () {
-            window.location.assign('../../sign-in')
-        }).catch(function (error) {
-            alert("Error occured")
+signOut
+  ? (signOut.onclick = () => {
+      firebase
+        .auth()
+        .signOut()
+        .then(function () {
+          window.location.assign("../../sign-in");
+        })
+        .catch(function (error) {
+          alert("Error occured");
         });
-    } :
-    null
+    })
+  : null;
