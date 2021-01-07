bde49c871ab817983547c799454d0491a67eec23
Ragan <lamocragan@gmail.com>
2021-01-06 20:12:23 -0800

    home course sort done

--- public/js/home/getCourses.js
+++ public/js/home/getCourses.js
@@ -1,6 +1,16 @@
+let allUserCourses = [];
+let inProgressUserCourses = [];
+let completedUserCourses = [];
+let latestCourses = [];
+const arrowLeftCat = document.getElementById("arrowLeftCat");
+const arrowRightCat = document.getElementById("arrowRightCat");
+const myCoursesContainer = document.getElementById("myCourses");
+const btnBadges = document.getElementsByClassName("badgeDev");
+let activeBadge = btnBadges[0];
+let currentUser;
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-    console.log(user);
+    currentUser = user;
     const latest = document.getElementById("latest");
     const arrowRight = document.getElementById("arrowRight");
     let currDate;
@@ -44,9 +54,60 @@ firebase.auth().onAuthStateChanged(function (user) {
                   if (data.course_thumbnail === undefined) {
                     newNode.firstElementChild.firstElementChild.removeAttribute("style");
                   }
+                  latestCourses.push(newNode);
+                  latestArrow(latestCourses);
                 });
               });
           });
+      });
+
+    firebase
+      .database()
+      .ref("student_user_course/" + user.uid)
+      .on("child_added", (snapshot) => {
+        let courseKey = snapshot.key;
+        let courseNode = document.createElement("div");
+        if (snapshot.val()) {
+          firebase
+            .database()
+            .ref("courses/" + courseKey)
+            .once("value")
+            .then((courseMain) => {
+              const courseMainData = courseMain.val();
+              // console.log(courseMainData);
+
+              courseNode.className = "col-6 col-lg-4 px-2 mt-2";
+              courseNode.setAttribute("key", courseMainData.course_id);
+              courseNode.setAttribute("onclick", `goToCourse(this)`);
+
+              courseNode.innerHTML = `
+                  <div class="card rounded courseCard">
+                    <div class="courseThumb" style="background: url(${courseMainData.course_thumbnail}) no-repeat center;"></div>
+                    <div class="card-body py-2 px-3">
+                      <p class="courseTitle mb-0">${courseMainData.course_title}</p>
+                      <p class="courseProf mb-2">${courseMainData.prof_name}</p>
+                      <p class="courseDes">
+                        ${courseMainData.course_brief}
+                      </p>
+                    </div>
+                  </div>`;
+
+              myCoursesContainer.insertBefore(courseNode, arrowRightCat);
+              if (courseMainData.course_thumbnail === undefined) {
+                courseNode.firstElementChild.firstElementChild.removeAttribute("style");
+              }
+              // myCoursesContainer.appendChild(courseNode);
+              allUserCourses.push(courseNode);
+
+              myCoursesArrow(allUserCourses);
+            });
+
+          // console.log(allUserCourses);
+        } else {
+          for (let i = 0; i < btnBadges.length; i++) {
+            btnBadges[i].setAttribute("disabled", "");
+          }
+        }
       });
   }
 });
@@ -67,4 +128,140 @@ firebase.auth().onAuthStateChanged(function (user) {
 function goToCourse(element) {
   courseId = element.getAttribute("key");
   window.location.assign(`/course?id=${courseId}`);
+}
+
+function changePage(element) {
+  if (activeBadge === element) {
+    return;
+  }
+  activeBadge.classList.remove("activeBadge");
+  activeBadge = element;
+  activeBadge.classList.add("activeBadge");
+
+  if (element.id === "all") {
+    elArrDnone(inProgressUserCourses);
+    elArrDnone(completedUserCourses);
+    elArrDisplay(allUserCourses);
+    myCoursesArrow(allUserCourses);
+  }
+
+  if (element.id === "inProgress") {
+    elArrDnone(allUserCourses);
+    elArrDnone(completedUserCourses);
+    if (inProgressUserCourses.length > 0) elArrDisplay(inProgressUserCourses);
+    else getInprogress();
+  }
+
+  if (element.id === "completed") {
+    elArrDnone(allUserCourses);
+    elArrDnone(inProgressUserCourses);
+    if (completedUserCourses.length > 0) elArrDisplay(completedUserCourses);
+    else getCompleted();
+  }
+}
+
+function getInprogress() {
+  firebase
+    .database()
+    .ref("student_user_course/" + currentUser.uid)
+    .on("child_added", (snapshot) => {
+      let data = snapshot.val();
+      let courseNode = document.createElement("div");
+
+      if (data.progress_percent < 100) {
+        firebase
+          .database()
+          .ref("courses/" + data.courseId)
+          .once("value")
+          .then((courseMain) => {
+            const courseMainData = courseMain.val();
+
+            courseNode.className = "col-6 col-lg-4 px-2 mt-2";
+            courseNode.setAttribute("key", courseMainData.course_id);
+            courseNode.setAttribute("onclick", `goToCourse(this)`);
+
+            courseNode.innerHTML = `
+                  <div class="card rounded courseCard">
+                    <div class="courseThumb" style="background: url(${courseMainData.course_thumbnail}) no-repeat center;"></div>
+                    <div class="card-body py-2 px-3">
+                      <p class="courseTitle mb-0">${courseMainData.course_title}</p>
+                      <p class="courseProf mb-2">${courseMainData.prof_name}</p>
+                      <p class="courseDes">
+                        ${courseMainData.course_brief}
+                      </p>
+                    </div>
+                  </div>`;
+
+            myCoursesContainer.insertBefore(courseNode, arrowRightCat);
+            if (courseMainData.course_thumbnail === undefined) {
+              courseNode.firstElementChild.firstElementChild.removeAttribute("style");
+            }
+            // myCoursesContainer.appendChild(courseNode);
+            inProgressUserCourses.push(courseNode);
+
+            myCoursesArrow(inProgressUserCourses);
+          });
+      }
+    });
+}
+
+function getCompleted() {
+  firebase
+    .database()
+    .ref("student_user_course/" + currentUser.uid)
+    .on("child_added", (snapshot) => {
+      let data = snapshot.val();
+      let courseNode = document.createElement("div");
+
+      if (data.progress_percent == 100) {
+        firebase
+          .database()
+          .ref("courses/" + data.courseId)
+          .once("value")
+          .then((courseMain) => {
+            const courseMainData = courseMain.val();
+
+            courseNode.className = "col-6 col-lg-4 px-2 mt-2";
+            courseNode.setAttribute("key", courseMainData.course_id);
+            courseNode.setAttribute("onclick", `goToCourse(this)`);
+
+            courseNode.innerHTML = `
+                  <div class="card rounded courseCard">
+                    <div class="courseThumb" style="background: url(${courseMainData.course_thumbnail}) no-repeat center;"></div>
+                    <div class="card-body py-2 px-3">
+                      <p class="courseTitle mb-0">${courseMainData.course_title}</p>
+                      <p class="courseProf mb-2">${courseMainData.prof_name}</p>
+                      <p class="courseDes">
+                        ${courseMainData.course_brief}
+                      </p>
+                    </div>
+                  </div>`;
+
+            myCoursesContainer.insertBefore(courseNode, arrowRightCat);
+            if (courseMainData.course_thumbnail === undefined) {
+              courseNode.firstElementChild.firstElementChild.removeAttribute("style");
+            }
+            // myCoursesContainer.appendChild(courseNode);
+            completedUserCourses.push(courseNode);
+
+            myCoursesArrow(completedUserCourses);
+          });
+      }
+    });
+}
+
+function elArrDnone(elementArr) {
+  for (element of elementArr) {
+    element.classList.add("d-none");
+  }
+  arrowRightCat.classList.remove("d-flex");
+  arrowLeftCat.classList.remove("d-flex");
+}
+
+function elArrDisplay(elementArr) {
+  for (element of elementArr) {
+    element.classList.remove("d-none");
+  }
+  // arrowRightCat.classList.remove("d-flex");
+  // arrowLeftCat.classList.remove("d-flex");
 }
